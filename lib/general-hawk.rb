b6329b8caa6552e7044d3c11264fabd617791957
Josh Owens <joshua.owens@gmail.com>
2011-02-10 10:41:44 -0500

    Get basic structure in place, still needs work.

--- lib/general-hawk.rb
+++ lib/general-hawk.rb
@@ -1,5 +1,25 @@
+require 'sinatra/base'
+require 'erb'
+
 module General
   module Hawk
-    # Your code goes here...
+    class Server < Sinatra::Base
+      
+      dir = File.dirname(File.expand_path(__FILE__))
+      
+      set :views,  "#{dir}/views"
+      set :public, "#{dir}/public"
+      
+      get "/" do
+        erb(:projects, {}, :projects => options.projects)
+      end
+      
+      
+      def self.rack_start(projects)
+        set :projects, projects
+        self.new
+      end
+      
+    end
   end
 end
